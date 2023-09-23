5a3f6bd99bdda8f04c924a6951a3ae21deea6dc1
rudnmk <1132226149@pfur.ru>
2023-09-24 01:43:33 +0300

    //header Лабораторная работа №1
    //body Сделал задания 1 и 2 лабораторной работы №1

---
+++ lab 1/main tasks/1/1/1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+C - C++
+strlen - strlen
+strcpy - strcpy
+strcmp - strcmp
+strcat - strcat
+someString[index] - someString[index]
+NONE - push_back
+NONE - pop_back
+
+Основная разница строк между С и С++ заключается в том, что в С строка - это массив символов(char[]), пока как в С++ - это отдельный элемент (std::string)
+*/
+
+
+int main() {
+    string str;
+    char temp;
+    cout << "Input your string: ";
+    getline(cin, str);
+    short len = str.length();
+    remove_if(str.begin(), str.end(), isspace);
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\nResult: " << str;
+
+    return 0;
+}
---
+++ lab 1/main tasks/2/2/2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+C - C++
+
+int *ptr = (int*)malloc(sizeof(int)) - int* arr = new int
+int *ptrArr = (int*)malloc(sizeof(int) * N) - new int[N]
+free(ptr) - delete ptr
+free(ptrArr) - delete[] ptr
+
+В языке C++ обязательно надо освобождать динамическую память для оптимизации работы программы. Если есть выделение памяти через new, значит, должен быть и delete.
+Нельзя освобождать одну и ту же выделенную память дважды. Освобождение уже несуществующей ячейки памяти может привести к непредсказуемым последствиям.
+Нельзя удалить невыделенную память, т.к. удалять нечего.
+
+*/
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	short rows;
+	short step = 1;
+	unsigned int number;
+	cout << "Введите число строк: ";
+	cin >> rows;
+	float length = ((1 + rows) * rows) / 2;
+	int* arr = new int[length];
+	int tmp1 = 1;
+	int tmp2 = 1;
+	*(arr) = tmp1;
+	*(arr + 1) = tmp2;
+	for (int i = 2; i < length; i++) {
+		*(arr + i) = tmp1 + tmp2;
+		if (i % 2 == 0) {
+			tmp1 = tmp1 + tmp2;
+		}
+		else {
+			tmp2 = tmp1 + tmp2;
+		}
+	}
+
+	cout << "Результат:\n";
+	int cnst = 0;
+	for (int i = 0; i < rows; i++) {
+		cout << string(rows - i, ' ');
+		for (int j = 0; j < step; j++) {
+			cout << *(arr + j + cnst) << " ";
+		}
+		cnst = cnst + step;
+		cout << "\n";
+		step++;
+	}
+	delete[] arr;
+	return 0;
+}
